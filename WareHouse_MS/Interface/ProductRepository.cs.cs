4ea5968185bc00742f97a2af613ae42dc35e3ba9
Hossein913 <Hosssein913@gmail.com>
2023-05-29 23:05:45 +0330

    Add ProductRepository

---
+++ WareHouse_MS/Interface/ProductRepository.cs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouse_MS.Domain;
+
+namespace WareHouse_MS.Interface
+{
+    public class ProductRepository : IProductRepository
+    {
+        public string AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProductList()
+        {
+            throw new NotImplementedException();
+        }
+        public bool CheckProductName(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

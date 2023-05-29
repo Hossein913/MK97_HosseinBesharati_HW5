c2c042b2b105b621eae55bb36878dcf2b00cfee4
Hossein913 <Hosssein913@gmail.com>
2023-05-29 21:27:04 +0330

    add IProductRepository

---
+++ WareHouse_MS/Interface/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouse_MS.Domain;
+
+namespace WareHouse_MS.Interface
+{
+    public interface IProductRepository
+    {
+        string AddProduct(Product product);
+        List<Product> GetProductList();
+        string GetProductById(int id);
+    }
+}

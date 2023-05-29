e172b8effc1cd8ed6a9a29232ba9a403272e64f5
Hossein913 <Hosssein913@gmail.com>
2023-05-29 20:27:20 +0330

    Add Domains

---
+++ WareHouse_MS/Domain/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouse_MS.Domain
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public int Barcode { get; set; }
+    }
+}
---
+++ WareHouse_MS/Domain/Stock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouse_MS.Domain
+{
+    public class Stock
+    {
+        public Guid StockId { get; set; }
+        public string Name { get; set; }
+        public Guid ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+        public int ProductPirce { get; set; }
+    }
+}

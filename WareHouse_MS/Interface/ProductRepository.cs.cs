dc1c8982d84dce73e499ab0e4a9d5ca13ca7738a
Hossein913 <Hosssein913@gmail.com>
2023-05-29 23:54:36 +0330

    Add and GetAll Methods in ProductRepository has Implemented

--- WareHouse_MS/Interface/ProductRepository.cs.cs
+++ WareHouse_MS/Interface/ProductRepository.cs.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,35 @@ namespace WareHouse_MS.Interface
 {
     public class ProductRepository : IProductRepository
     {
+        private static string workingDirectory = Environment.CurrentDirectory;
+        private static string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+        private static string Database = String.Concat(projectDirectory, "\\Database\\ProductJson.json");
+        
+
         public string AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            string SerializedString;
+            if (CheckProductName(product.Name))
+            {
+
+                List<Product> products = GetProductList();
+                if (products == null)
+                {
+                    products = new List<Product>();
+                }
+
+                products.Add(product);
+                SerializedString = JsonConvert.SerializeObject(products);
+
+                using (StreamWriter writer = new StreamWriter(Database))
+                    writer.Write(SerializedString);
+
+                return product.Name;
+
+
+            }
+
+            return null;
         }
 
         public string GetProductById(int id)
@@ -21,11 +49,23 @@ namespace WareHouse_MS.Interface
 
         public List<Product> GetProductList()
         {
-            throw new NotImplementedException();
+            CheckDatabase(Database);
+            string DatabaseString = File.ReadAllText(Database);
+            var products = JsonConvert.DeserializeObject<List<Product>>(DatabaseString);
+            return products;
         }
         public bool CheckProductName(string productName)
         {
             throw new NotImplementedException();
+        }
+
+
+        private void CheckDatabase(string Directory)
+        {
+            if (!File.Exists(Directory))
+            {
+                File.Create(Directory);
+            }
         }
 
     }

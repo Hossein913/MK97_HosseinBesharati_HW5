7bd72e81f68f6a9f03efc797cbe491d60abfa446
Hossein913 <Hosssein913@gmail.com>
2023-06-04 19:03:52 +0330

    Add CustomException.cs with ProductNotFound and NotEnoughQuantity exceptions

---
+++ WareHouse_MS/CustomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouse_MS
+{
+    public class ProductNotFound : SystemException
+    {
+        public ProductNotFound(string msg) : base(msg) { }
+        public ProductNotFound() : base() { }
+
+    }
+
+    public class NotEnoughQuantity : SystemException
+    {
+        public NotEnoughQuantity(string msg) : base(msg) { }
+
+    }
+}

e84f860340ee51f8b8066022dc370842a20bc3d2
Hossein913 <Hosssein913@gmail.com>
2023-06-04 16:31:40 +0330

    Add common static class

---
+++ WareHouse_MS/Common.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouse_MS
+{
+    public static class Common
+    {
+        public static string GetProjectDirectory()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.FullName;
+            return projectDirectory;
+        }
+
+        public static string GetProjectDirectory(string Filename)
+        {
+            string FullDirectory = String.Concat(GetProjectDirectory(), Filename);
+            return FullDirectory;
+        }
+    }
+}

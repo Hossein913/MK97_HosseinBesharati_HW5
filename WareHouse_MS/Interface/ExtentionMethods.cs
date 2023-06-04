920d6bed967b5ea7c2dfe063855996bf205dbdc2
Hossein913 <Hosssein913@gmail.com>
2023-06-04 16:46:28 +0330

    Add Extention Methods class

---
+++ WareHouse_MS/Interface/ExtentionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouse_MS.Interface
+{
+    public static class ExtentionMethods
+    {
+        /// <summary>
+        /// helps to Write a text Into a file
+        /// </summary>
+        /// <param name="FileDirectory">Start file derectory from project location</param>
+        public static void WriteOnFile(this string text, string FileDirectory)
+        {
+            using (StreamWriter writer = new StreamWriter(Common.GetProjectDirectory(FileDirectory)))
+            { writer.Write(text); }
+
+
+        }
+    }
+}

61d3417c0550b4b920882c5759e54891f9d4a7b1
Hossein913 <Hosssein913@gmail.com>
2023-06-04 17:25:27 +0330

    Add IStockRepositoy.cs and StockRepository.cs to project

---
+++ WareHouse_MS/Interface/IStockRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouse_MS.Domain;
+
+namespace WareHouse_MS.Interface
+{
+    public interface IStockRepository
+    {
+        string SaleProduct(int productId, int cnt);
+        string BuyProduct(Stock productInStock);
+        List<Stock> GetSalesProductList();
+
+    }
+}
---
+++ WareHouse_MS/Interface/StockRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouse_MS.Domain;
+
+namespace WareHouse_MS.Interface
+{
+    public class StockRepository : IStockRepository
+    {
+
+        private IProductRepository productRepository;
+
+        public StockRepository(IProductRepository ProductRepo)
+        {
+            productRepository = ProductRepo;
+        }
+
+        public string BuyProduct(Stock productInStock)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Stock> GetSalesProductList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SaleProduct(int productId, int cnt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

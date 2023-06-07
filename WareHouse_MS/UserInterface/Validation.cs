0713bcccd5156e672845b9820a3ab4164a480893
Hossein913 <Hosssein913@gmail.com>
2023-06-07 22:34:38 +0330

    add validation class and add menucontrol and CheckProductname methods on it

---
+++ WareHouse_MS/UserInterface/Validation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouse_MS.UserInterface
+{
+
+    public static class Validation
+    {
+        public static bool MenuInputValidation(string input, int endnum)
+        {
+
+            int menuInput;
+            bool flag = false;
+
+            flag = int.TryParse(input.Trim(), out menuInput);
+            if (!flag)
+            {
+                throw new InvalidCastException("Input is not a number");
+            }
+            if (flag = true && (menuInput < 1 || menuInput > endnum))
+            {
+                flag = false;
+                throw new ArgumentOutOfRangeException("input number is out of Range");
+            }
+            return flag;
+
+        }
+
+
+        public static bool CheckProductName(string productName)
+        {
+            return true;
+        }
+
+    }
+}
